a1e7b295fcf9e0ffa7c311e667552871a5403cc6
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2025-07-22 09:40:49 +0530

    fixed error and complete project

--- expense-tracker-api/server.js
+++ expense-tracker-api/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(core());
 
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/transactions', require('./routes/transactions'));
+app.use('/api/transactions', require('./routes/transaction'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
